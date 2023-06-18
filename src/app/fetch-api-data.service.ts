f7f22daf7c3e5d8046173f2845ec6c963e6f8db7
Schlumen <lumen.haendler@web.de>
2023-06-18 19:26:06 +0200

    Add login and registration form

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -9,7 +9,7 @@ const apiUrl = 'https://myflixapi-11d1.onrender.com/';
     providedIn: 'root'
 })
 
-export class UserRegistrationService {
+export class FetchApiDataService {
     // Inject the HttpClient module to the constructor params
     // This will provide HttpClient to the entire class, making it available via this.http
 
